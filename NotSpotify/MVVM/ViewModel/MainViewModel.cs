ebd59d13aac36853d40eedde58792e165b1de3cd
willyhuang <willyhuang@flytech.com.tw>
2023-02-22 10:21:32 +0800

    first commit

---
+++ NotSpotify/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using NotSpotify.MVVM.Model;
+using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotSpotify.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Item> Songs { get; set; }
+
+        public MainViewModel()
+        {
+            Songs = new ObservableCollection<Item>();
+            PopulateCollection();
+        }
+
+        void PopulateCollection()
+        {
+            var client = new RestClient();
+            client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("", "Bearer");
+
+            var request = new RestRequest("https://api.spotify.com/v1/browse/new-releases", Method.Get);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+
+            var response = client.GetAsync(request).GetAwaiter().GetResult();
+            var data = JsonConvert.DeserializeObject<TrackModel>(response.Content);
+
+            for (int i = 0; i < data.Albums.Limit; i++)
+            {
+                var track = data.Albums.Items[i];
+                track.Duration = "2:32";
+                Songs.Add(track);
+            }
+        }
+    }
+}
